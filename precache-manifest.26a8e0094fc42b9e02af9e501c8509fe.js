5bcd472faeefd42eacfe1ef6f8271ffecbde394b
Ieva Vyliaudaite <ievavyl@yahoo.com>
2020-02-18 12:48:55 +0200

    Updates

---
+++ precache-manifest.26a8e0094fc42b9e02af9e501c8509fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7ca8a3d84d03f748681dab0a29d950",
+    "url": "/farmers-database/index.html"
+  },
+  {
+    "revision": "29bb3d5433632643cda7",
+    "url": "/farmers-database/static/css/main.3c330968.chunk.css"
+  },
+  {
+    "revision": "9d412ad0ecd11f365f51",
+    "url": "/farmers-database/static/js/2.2cb7bc94.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/farmers-database/static/js/2.2cb7bc94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29bb3d5433632643cda7",
+    "url": "/farmers-database/static/js/main.2cfb6968.chunk.js"
+  },
+  {
+    "revision": "8e63633572cab975ec98",
+    "url": "/farmers-database/static/js/runtime-main.882ea754.js"
+  }
+]);
