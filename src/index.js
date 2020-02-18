615ac160503f6531590eb5fd78c334141ea5cc04
Ieva Vyliaudaite <ievavyl@yahoo.com>
2020-02-18 14:54:38 +0200

    edit README

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import store from "./js/store/store";
 import App from "./js/components/App";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById("root")
+		<Provider store={store}>
+				<App />
+		</Provider>,
+		document.getElementById("root")
 );
